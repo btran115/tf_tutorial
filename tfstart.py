06ba770cc288a3aedb649966511aa12e3c7d9225
btran115 <btran115@mit.edu>
2017-03-26 16:17:05 -0400

    here's the start file

---
+++ tfstart.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+sess = tf.Session()
+W = tf.Variable([.3], tf.float32)
+b = tf.Variable([-.3], tf.float32)
+x = tf.placeholder(tf.float32)
+linear_model = W*x+b
+
+init = tf.global_variables_initializer()
+sess.run(init)
+
+print(sess.run(linear_model, {x: [1,2,3,4]}))
+
+y = tf.placeholder(tf.float32)
+squared_deltas = tf.square(linear_model - y)
+loss = tf.reduce_sum(squared_deltas)
+print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
+
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+
+sess.run(init)
+for i in range(10000):
+	sess.run(train, {x:[1,2,3,4], y:[0,-1,-2,-3]})
+
+print(sess.run([W,b]))
